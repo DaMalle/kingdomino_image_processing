dcc6c8f1926a4b34c1f2af7b3fd991ca152725b0
Damalle <marcfriis@protonmail.com>
2023-09-26 10:13:35 +0200

    divided image into 25 smaller images

--- kingdomino.py
+++ kingdomino.py
@@ -1,24 +1,12 @@
 import cv2 as cv
-import numpy as np
-
-
-def divide_img_blocks(img, number_blocks=(2, 2)):
-    horizontal = np.array_split(img, number_blocks[0])
-    splitted_img = [np.array_split(block, number_blocks[1], axis=1) for block in horizontal]
-    return np.asarray(splitted_img, dtype=np.ndarray).reshape(number_blocks)
 
 
 def main() -> None:
     img = cv.imread("1.jpg")
-    height, width, channels = img.shape
-    # img = img[height//2, :width//2:]
-    # img = divide_img_blocks(img)
-    # for i in range(img.shape[0]):
-    #     for j in range(img.shape[1]):
-    #         cv.imwrite("test", img[i,j])
-    # cv.imshow("test", img)
-    left = img[:height//5, :width//5]
-    cv.imshow("test", left)
+    for i in range(0, 5):
+        for j in range(0, 5):
+            current_img = img[100*i:100*(i+1), 100*j:100*(j+1)]
+            cv.imshow(f"test{i}{j}.jpg", current_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
